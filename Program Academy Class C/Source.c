54759110ffa013ad22884b47904b8bab420c3540
haaqua <hash_toma@icloud.com>
2026-01-15 21:44:18 +0900

    (2026.1.15) 이중 버퍼링

--- Program Academy Class C/Source.c
+++ Program Academy Class C/Source.c
@@ -1,5 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#define SIZE 10000
+
 int main() {
 #pragma region 파일 입출력
 
@@ -8,10 +10,35 @@ int main() {
 	// 첫 번째 매개 변수 (파일의 이름)
 	// 두 번째 매개 변수 (파일의 입출력 모드)
 
-	FILE* file = fopen("data.txt", "w");
-	fputs("character info\nhp : \nmp : \nstr : \ndex : \nwis : ", file);
-	fclose(file);
+	//FILE* file = fopen("data.txt", "w");
+	//fputs("character info\nhp : \nmp : \nstr : \ndex : \nwis : ", file);
+	//fclose(file);
 
+#pragma endregion
+
+#pragma region 파일 읽기
+
+	FILE* file = fopen("data.txt", "r");
+
+	int count = 0;
+
+	char character = NULL;
+	while (character=fgetc(file) != EOF) {
+		count++;
+	}
+	printf("count : %d\n", count);
+
+	char buffer[SIZE] = {" "};
+
+	// 첫 번째 매개변수 : 읽은 데이터를 저장할 메모리 버퍼의 포인터 변수
+	// 두 번쨰 매개변수 : 각 데이터 항목의 크기
+	// 세 번째 매개변수 : 데이터를 읽어올 항목의 수
+	// 네 번째 매개변수 : 데이터를 읽어올 파일의 포인터 변수
+	fread(buffer,sizeof(char), sizeof(buffer), file);
+	rewind(file);
+	fread(buffer, sizeof(char), sizeof(buffer), file);
+	printf("%s\n",buffer);
+	fclose(file);
 #pragma endregion
 
 
