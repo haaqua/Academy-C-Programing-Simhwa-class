74d1c735db18990e9b965dfb8796b39903e67ab3
haaqua <hash_toma@icloud.com>
2026-01-16 21:44:05 +0900

    (2026.1.16) 색상 시스템

--- Program Academy Class C/Source.c
+++ Program Academy Class C/Source.c
@@ -1,7 +1,35 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#define SIZE 10000
+#include<stdlib.h>
 
+
+void load(const char* filename) {
+
+	FILE* file = fopen(filename, "r");
+
+	int count = 0;
+
+	int character = '\0';
+	while (character = fgetc(file) != EOF) {
+		count++;
+	}
+
+	rewind(file);
+
+	char* buffer = malloc(count + 1);
+
+	buffer[count] = NULL;
+
+	// 첫 번째 매개변수 : 읽은 데이터를 저장할 메모리 버퍼의 포인터 변수
+	// 두 번쨰 매개변수 : 각 데이터 항목의 크기
+	// 세 번째 매개변수 : 데이터를 읽어올 항목의 수
+	// 네 번째 매개변수 : 데이터를 읽어올 파일의 포인터 변수
+	fread(buffer, sizeof(char), count, file);
+	printf("%s\n", buffer);
+	fclose(file);
+
+	free(buffer);
+}
 int main() {
 #pragma region 파일 입출력
 
@@ -16,30 +44,11 @@ int main() {
 
 #pragma endregion
 
-#pragma region 파일 읽기
-
-	FILE* file = fopen("data.txt", "r");
-
-	int count = 0;
-
-	char character = NULL;
-	while (character=fgetc(file) != EOF) {
-		count++;
-	}
-	printf("count : %d\n", count);
-
-	char buffer[SIZE] = {" "};
-
-	// 첫 번째 매개변수 : 읽은 데이터를 저장할 메모리 버퍼의 포인터 변수
-	// 두 번쨰 매개변수 : 각 데이터 항목의 크기
-	// 세 번째 매개변수 : 데이터를 읽어올 항목의 수
-	// 네 번째 매개변수 : 데이터를 읽어올 파일의 포인터 변수
-	fread(buffer,sizeof(char), sizeof(buffer), file);
-	rewind(file);
-	fread(buffer, sizeof(char), sizeof(buffer), file);
-	printf("%s\n",buffer);
-	fclose(file);
+#pragma region 파일읽기
+	//load("resource/goblin.txt");
+	//load("resource/Slime.txt");
 #pragma endregion
+
 
 
 #pragma endregion
