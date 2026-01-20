4d72b1f82e5d6360bb4e25b4a3d65dbfd76b4a78
haaqua <hash_toma@icloud.com>
2026-01-20 21:43:42 +0900

    (2026.01.20) 열거형

--- Program Academy Class C/Source.c
+++ Program Academy Class C/Source.c
@@ -1,7 +1,47 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<Windows.h>
 
+
+#define UP 72
+#define	LEFT 75
+#define RIGHT 77
+#define DOWN 80
+
+void position(int x, int y) {
+	COORD position = { x,y };
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+}
 int main() 
 {
+	int x = 5, y = 5;
+	//position(5, 5);
+	//printf("¡Ú");
 
+	int key = 0;
+	while (1) {
+		key = _getch();
+		if(key == 0|| key == 224){
+			key = _getch();
+		}
+
+		switch (key) {
+		case UP:y--;
+			break;
+		case LEFT:x-=2;
+			break;
+		case RIGHT:x+=2;
+			break;
+		case DOWN:y++;
+			break;
+		default:printf("exception\n");
+			break;
+		}
+		system("cls");
+		position(x, y);
+		printf("¡Ú");
+	}
+
+	return 0;
 }
